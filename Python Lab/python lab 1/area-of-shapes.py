ca0bab94d4ac08625fda53574ae11bbfeb612199
Sandeep Kapoor <111681462+sandeep1077@users.noreply.github.com>
2023-04-03 20:26:48 +0530

    Delete Python Lab/python lab 1 directory

--- Python Lab/python lab 1/area-of-shapes.py
+++
@@ -1,9 +0,0 @@
-a= eval(input("enter side of square\n"))
-print("area of square is :", a*a)
-b=eval(input("enter length of rectangle\n"))
-c=eval(input("enter bredth of rectangle\n"))
-print("area of rectangle is :",b*c)
-d=eval(input("enter radius of circle\n"))
-pi=3.14
-print("area of circle is :",pi*d*d)
-
--- Python Lab/python lab 1/currency-conversion.py
+++
@@ -1,5 +0,0 @@
-a=eval (input("enter amount in indian rupee:"))
-us=a/82.70
-uk=a/99.62
-print("enter amount in us dollar is :", us ,"us dollars")
-print("enter amount in uk pound  is :",uk,"uk pound ")
